425f7e07daf2b3bd8dc586e23c768b5ff39a062a
Kiu Ai <aij914@gmail.com>
2021-05-24 14:51:49 +0800

    init commit.

---
+++ apple-login/apple_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_loginApp.swift
+//  apple-login
+//
+//  Created by Alvin Ai on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct apple_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
